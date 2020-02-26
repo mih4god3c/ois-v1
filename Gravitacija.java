a6ccf9a81a39cc01250991a0706d6830375b7e8a
lige8 <0abcdefghijklmnopqrstuvwxyz27@gmail.com>
2020-02-26 17:23:03 +0000

    Dodajanje izračuna pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,9 +1,16 @@
 import java.util.*;
 public class Gravitacija {
     public static void main (String[]args) {
+
         
-        System.out.println("OIS JE ZAKON");
         
+    }
+    public int gravitacijskiPospesek (int nadmorskaVisina){
+        float konstanta = 6,674 * Math.pow(10, -11);
+        float masaZemlje = 5,972 * Math.pow(10, 24);
+        float polmerZemlje = 6,731 * Math.pow(10, 6);
+        float gravitacijskiPospesek = (konstanta * masaZemlje) / (Math.pow((polmerZemlje + nadmorskaVisina), 2));
+        return gravitacijskiPospesek;
         
     }
 }
