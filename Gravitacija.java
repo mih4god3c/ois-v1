f59987bd0c7c5898d76789aa9c5fa0776f14bbad
mih4god3c <mg3229@student.uni-lj.si>
2020-02-26 18:15:30 +0000

    Končna verzija

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,21 +2,23 @@ import java.util.*;
 public class Gravitacija {
     public static void main (String[]args) {
 
-        
-
+    Scanner sc = new Scanner(System.in);
+    double nadmorskaVisina = sc.nextInt();
+    double gravitacijskiPospesek = gravitacijskiPospesek(nadmorskaVisina);
+    izpis(nadmorskaVisina,gravitacijskiPospesek);
         
     }
-    public int gravitacijskiPospesek (int nadmorskaVisina){
-        float konstanta = 6,674 * Math.pow(10, -11);
-        float masaZemlje = 5,972 * Math.pow(10, 24);
-        float polmerZemlje = 6,731 * Math.pow(10, 6);
-        float gravitacijskiPospesek = (konstanta * masaZemlje) / (Math.pow((polmerZemlje + nadmorskaVisina), 2));
+    public static double gravitacijskiPospesek (double nadmorskaVisina){
+        double konstanta = 6.674 * Math.pow(10, -11);
+        double masaZemlje = 5.972 * Math.pow(10, 24);
+        double polmerZemlje = 6.371 * Math.pow(10, 6);
+        double gravitacijskiPospesek = (konstanta * masaZemlje) / (Math.pow((polmerZemlje + nadmorskaVisina), 2));
         return gravitacijskiPospesek;
         
 
     }
-    public static int izpis (int podanaVisina){
-        System.out.println(podanaVisina)';
+    public static void izpis (double podanaVisina, double gravitacijskiPospesek){
+        System.out.println(podanaVisina);
         System.out.println(gravitacijskiPospesek);
 
     }
